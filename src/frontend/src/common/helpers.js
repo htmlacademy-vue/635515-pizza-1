0f095549d5602b88e99baa73c686a40d890eef1e
Musisimaru <musisimaru@yandex.ru>
2021-11-20 02:28:24 +0300

    feat: add dynamic ingredients

--- src/frontend/src/common/helpers.js
+++ src/frontend/src/common/helpers.js
@@ -6,3 +6,12 @@
     internalName: regexResult.length > 1 ? regexResult[1] : "",
   };
 };
+
+export const extendIngredient = (ingredient) => {
+  const { image } = ingredient;
+  const regexResult = /(\w*).svg/.exec(image);
+  return {
+    ...ingredient,
+    internalName: regexResult.length > 1 ? regexResult[1] : "",
+  };
+};
