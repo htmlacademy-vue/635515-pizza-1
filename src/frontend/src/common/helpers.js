65cf9bcdf4cfdf1f8a4722a3ca8c4bd8d2ed704f
Musisimaru <musisimaru@yandex.ru>
2021-11-20 01:31:07 +0300

    feat: add dynamic dough

---
+++ src/frontend/src/common/helpers.js
@@ -0,0 +1,8 @@
+﻿export const extendDough = (dough) => {
+  const { image } = dough;
+  const regexResult = /dough-(\w*).svg/.exec(image);
+  return {
+    ...dough,
+    internalName: regexResult.length > 1 ? regexResult[1] : "",
+  };
+};
