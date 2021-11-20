e28253e78f907f462cd1a3b064a796d98cf5048d
Musisimaru <musisimaru@yandex.ru>
2021-11-20 19:31:00 +0300

    feat: module 01 task 01 (#2)
    * feat: add dynamic dough
    
    * feat: add dynamic sizes
    
    * feat: add dynamic sauces
    
    * feat: add dynamic ingredients
    
    * refactor: removed unnecessary
    
    * fix: return enums and initial json
    
    Co-authored-by: Unknown Keks <keksobot@users.noreply.github.com>

---
+++ src/frontend/src/views/Index.vue
@@ -0,0 +1,185 @@
+﻿<template>
+  <section>
+    <main class="content">
+      <form action="#" method="post">
+        <div class="content__wrapper">
+          <h1 class="title title--big">Конструктор пиццы</h1>
+
+          <div class="content__dough">
+            <div class="sheet">
+              <h2 class="title title--small sheet__title">Выберите тесто</h2>
+
+              <div class="sheet__content dough">
+                <label
+                  v-for="dough in doughOptions"
+                  :key="dough.id"
+                  class="dough__input"
+                  :class="`dough__input--${dough.internalName}`"
+                >
+                  <input
+                    type="radio"
+                    name="dought"
+                    :value="dough.internalName"
+                    class="visually-hidden"
+                  />
+                  <b>{{ dough.name }}</b>
+                  <span>{{ dough.description }}</span>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="content__diameter">
+            <div class="sheet">
+              <h2 class="title title--small sheet__title">Выберите размер</h2>
+
+              <div class="sheet__content diameter">
+                <label
+                  v-for="size in sizes"
+                  :key="size.id"
+                  class="diameter__input"
+                  :class="`diameter__input--${size.internalName}`"
+                >
+                  <input
+                    type="radio"
+                    name="diameter"
+                    :value="size.internalName"
+                    class="visually-hidden"
+                  />
+                  <span>{{ size.name }}</span>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="content__ingredients">
+            <div class="sheet">
+              <h2 class="title title--small sheet__title">
+                Выберите ингредиенты
+              </h2>
+
+              <div class="sheet__content ingredients">
+                <div class="ingredients__sauce">
+                  <p>Основной соус:</p>
+
+                  <label
+                    v-for="sauce in sauces"
+                    :key="sauce.id"
+                    class="radio ingredients__input"
+                  >
+                    <input
+                      type="radio"
+                      name="sauce"
+                      :value="sauce.internalName"
+                    />
+                    <span>{{ sauce.name }}</span>
+                  </label>
+                </div>
+
+                <div class="ingredients__filling">
+                  <p>Начинка:</p>
+
+                  <ul class="ingredients__list">
+                    <li
+                      v-for="ingredient in ingredients"
+                      :key="ingredient.id"
+                      class="ingredients__item"
+                    >
+                      <span
+                        class="filling"
+                        :class="`filling--${ingredient.internalName}`"
+                        >{{ ingredient.name }}</span
+                      >
+
+                      <div class="counter counter--orange ingredients__counter">
+                        <button
+                          type="button"
+                          class="counter__button counter__button--minus"
+                          disabled
+                        >
+                          <span class="visually-hidden">Меньше</span>
+                        </button>
+                        <input
+                          type="text"
+                          name="counter"
+                          class="counter__input"
+                          value="0"
+                        />
+                        <button
+                          type="button"
+                          class="counter__button counter__button--plus"
+                        >
+                          <span class="visually-hidden">Больше</span>
+                        </button>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="content__pizza">
+            <label class="input">
+              <span class="visually-hidden">Название пиццы</span>
+              <input
+                type="text"
+                name="pizza_name"
+                placeholder="Введите название пиццы"
+              />
+            </label>
+
+            <div class="content__constructor">
+              <div class="pizza pizza--foundation--big-tomato">
+                <div class="pizza__wrapper">
+                  <div class="pizza__filling pizza__filling--ananas"></div>
+                  <div class="pizza__filling pizza__filling--bacon"></div>
+                  <div class="pizza__filling pizza__filling--cheddar"></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="content__result">
+              <p>Итого: 0 ₽</p>
+              <button type="button" class="button" disabled>Готовьте!</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </main>
+  </section>
+</template>
+
+<script>
+import misc from "@/static/misc.json";
+import pizza from "@/static/pizza.json";
+import user from "@/static/user.json";
+import { extendDough, extendIngredient } from "@/common/helpers";
+import SauceNames from "../common/enums/sauceNames";
+import SizeNames from "../common/enums/sizeNames";
+
+export default {
+  name: "IndexHome",
+  data() {
+    return {
+      misc,
+      pizza,
+      user,
+      sauces: pizza.sauces.map((sauce) => ({
+        ...sauce,
+        internalName: SauceNames[sauce.name],
+      })),
+      sizes: pizza.sizes.map((size) => ({
+        ...size,
+        internalName: SizeNames[size.multiplier],
+      })),
+      ingredients: pizza.ingredients.map((ingredient) =>
+        extendIngredient(ingredient)
+      ),
+      doughOptions: pizza.dough.map((doughItem) => extendDough(doughItem)),
+    };
+  },
+};
+</script>
+
+<style></style>
