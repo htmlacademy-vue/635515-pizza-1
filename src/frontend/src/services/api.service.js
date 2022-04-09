ff9a516f083364c2bdfc8ba8920a845498c77b8d
Musisimaru <musisimaru@yandex.ru>
2022-04-09 14:32:07 +0300

    build: init changes

---
+++ src/frontend/src/services/api.service.js
@@ -0,0 +1,5 @@
+﻿// import JwtService from '@/services/jwt.service';
+// import taskStatuses from '@/common/enums/taskStatuses';
+// import timeStatuses from '@/common/enums/timeStatuses';
+// import { DAY_IN_MILLISEC } from '@/common/constants';
+// import axios from '@/plugins/axios';
---
+++ src/frontend/src/services/jwt.service.js
@@ -0,0 +1,16 @@
+﻿// ключ, по которому мы сохраним токен в LocalStorage
+const ID_TOKEN_KEY = "pz_token";
+
+export const getToken = () => {
+  return window.localStorage.getItem(ID_TOKEN_KEY);
+};
+
+export const saveToken = (token) => {
+  window.localStorage.setItem(ID_TOKEN_KEY, token);
+};
+
+export const destroyToken = () => {
+  window.localStorage.removeItem(ID_TOKEN_KEY);
+};
+
+export default { getToken, saveToken, destroyToken };
