fddbe7008f2f8e1c7cb2b685c3204b9f9ce40c29
Musisimaru <musisimaru@yandex.ru>
2022-04-09 16:30:34 +0300

    fix: removed traces from a neighboring project

--- src/frontend/src/plugins/axios.js
+++ src/frontend/src/plugins/axios.js
@@ -1,4 +1,5 @@
 ﻿import axios from "axios";
+import { hiddenError } from "@/common/helpers";
 
 const axiosInstance = axios.create({
   baseURL: "/api/",
@@ -8,9 +9,7 @@ axiosInstance.interceptors.response.use(
   (res) => res,
   (e) => {
     const defaultMessage = "Возникла ошибка при выполнении запроса к серверу";
-    axiosInstance.$notifier.error(
-      e?.response?.data?.error?.message || defaultMessage
-    );
+    hiddenError(e?.response?.data?.error?.message || defaultMessage);
     return Promise.reject(e);
   }
 );
--- src/frontend/src/services/api.service.js
+++ src/frontend/src/services/api.service.js
@@ -1,5 +1,49 @@
-﻿// import JwtService from '@/services/jwt.service';
-// import taskStatuses from '@/common/enums/taskStatuses';
-// import timeStatuses from '@/common/enums/timeStatuses';
-// import { DAY_IN_MILLISEC } from '@/common/constants';
-// import axios from '@/plugins/axios';
+﻿// import JwtService from "@/services/jwt.service";
+import axios from "@/plugins/axios";
+
+class BaseApiService {
+  constructor() {}
+}
+
+export class ReadOnlyApiService extends BaseApiService {
+  #resource;
+
+  constructor(resource) {
+    super();
+    this.#resource = resource;
+  }
+
+  async query(config = {}) {
+    const { data } = await axios.get(this.#resource, config);
+    return data;
+  }
+
+  async get(id, config = {}) {
+    const { data } = await axios.get(`${this.#resource}/${id}`, config);
+    return data;
+  }
+}
+
+export class CrudApiService extends ReadOnlyApiService {
+  #resource;
+
+  constructor(resource) {
+    super(resource);
+    this.#resource = resource;
+  }
+
+  async post(entity) {
+    const { data } = await axios.post(this.#resource, entity);
+    return data;
+  }
+
+  async put(entity) {
+    const { data } = await axios.put(`${this.#resource}/${entity.id}`, entity);
+    return data;
+  }
+
+  async delete(id) {
+    const { data } = await axios.delete(`${this.#resource}/${id}`);
+    return data;
+  }
+}
